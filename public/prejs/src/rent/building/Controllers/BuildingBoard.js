df2e90335f40c4e3730c8e342bf75576f85f4aba
chenxiaobinmd2012@gmail.com <chenxiaobinmd2012@gmail.com>
2015-12-06 00:04:23 +0800

    add function start

--- public/prejs/src/rent/building/Controllers/BuildingBoard.js
+++ public/prejs/src/rent/building/Controllers/BuildingBoard.js
@@ -6,8 +6,10 @@ angular.module('Rent.Building')
       // buildboard.AllBuildings = BuildingModel.AllBuildings();
       // var b = BuildingModel.restResource.get(1);
       // console.log(BuildingModel.restResource);
+      var allBuildings;
       BuildingModel.restResource.getList().then(function(buildings){
         buildboard.AllBuildings = buildings;
+        allBuildings = buildings;
       });
 
       buildboard.openmodal = function(aBuilding) {
@@ -19,6 +21,26 @@ angular.module('Rent.Building')
               building:aBuilding
             }
           });
+      };
+
+      buildboard.openAddModal = function() {
+          $uibModal.open({
+            templateUrl: "/view/rent/building/buildingAddModal.html",
+            controller: "BuildingAddModalCtrl",
+            controllerAs: "buildingAddModal",
+          });
+      };
+
+      buildboard.deleteBuilding = function (aBuilding) {
+        if(confirm("确定删除?")) {
+          aBuilding.remove().then(function(){
+            var index = buildboard.AllBuildings.indexOf(aBuilding);
+            if (index > -1) {
+              buildboard.AllBuildings.splice(index, 1);
+            }
+          });
+
+        }
       };
 /*
       var test = BuildingModel.restResource.one(1).get().then(function(aa){
@@ -50,32 +72,21 @@ angular.module('Rent.Building')
             building.save();
             $uibModalInstance.close();
           };
-
-          buildingModal.delete = function(){
-              building.remove();
-              $uibModalInstance.close();
-          };
         }
       );
-/*
-      angular.module('Rent.Building')
-        .controller('BuildingModalAddCtrl',
-          function($uibModalInstance, building) {
-            var buildingModal = this;
-            buildingModal.newBuilding = {};
 
-            buildingModal.cancel = function(){
+    angular.module('Rent.Building')
+        .controller('BuildingAddModalCtrl',
+          function($uibModalInstance) {
+            var buildingAddModal = this;
+            buildingAddModal.newbuilding = {name:"", rooms_count:0, desc:""};
+
+            buildingAddModal.cancel = function(){
               $uibModalInstance.close();
             };
 
-            buildingModal.save = function(){
-              building.name = buildingModal.building.name;
-              building.rooms_count = buildingModal.building.rooms_count;
-              building.desc = buildingModal.building.desc;
-              building.save();
+            buildingAddModal.save = function(){
               $uibModalInstance.close();
             };
-
-        }
-        );
-*/
+          }
+      );
