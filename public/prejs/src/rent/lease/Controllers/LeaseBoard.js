32d1594ff4d724ecbbf40bc11de621a7f1d8a08d
chenxiaobinmd2012@gmail.com <chenxiaobinmd2012@gmail.com>
2015-12-11 18:16:09 +0800

    Add lease building detail dialog

--- public/prejs/src/rent/lease/Controllers/LeaseBoard.js
+++ public/prejs/src/rent/lease/Controllers/LeaseBoard.js
@@ -2,8 +2,31 @@ angular.module('Rent.Lease')
   .controller('LeaseboardCtrl',
     function($uibModal, LeaseModel) {
       var leaseboard = this;
-      console.log("yes");
       LeaseModel.restResource.getList().then(function(buildings){
         leaseboard.AllBuildings = buildings;
       });
+
+      leaseboard.openBuildingLeaseInfo = function(aBuilding) {
+        $uibModal.open({
+          templateUrl: "/view/rent/lease/LeaseBuildingDetailModal.html",
+          controller: "LeaseBuildingDetailModalCtrl",
+          controllerAs: "leaseBuildingModal",
+          resolve: {
+            building:aBuilding
+          }
+        });
+      }
     });
+
+
+    angular.module('Rent.Lease')
+      .controller('LeaseBuildingDetailModalCtrl',
+        function($uibModalInstance, building) {
+          var leaseBuildingModal = this;
+          leaseBuildingModal.building = building;
+
+          leaseBuildingModal.close = function(){
+            $uibModalInstance.close();
+          };
+        }
+      );
